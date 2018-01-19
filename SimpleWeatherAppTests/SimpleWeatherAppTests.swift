a863762e808cd68f42da8998d1a2988352c7463a
Justyna Dolińska <justyna@secansoida.com>
2018-01-19 13:59:29 +0100

    Initial Commit

---
+++ SimpleWeatherAppTests/SimpleWeatherAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  SimpleWeatherAppTests.swift
+//  SimpleWeatherAppTests
+//
+//  Created by Justyna Dolińska on 19/01/2018.
+//  Copyright © 2018 Justyna Dolińska. All rights reserved.
+//
+
+import XCTest
+@testable import SimpleWeatherApp
+
+class SimpleWeatherAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
