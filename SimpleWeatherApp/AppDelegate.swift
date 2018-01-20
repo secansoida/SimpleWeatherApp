b5b9dc6c346cd025e3facbeb704f29b196c84003
Justyna Dolińska <justyna@secansoida.com>
2018-01-20 18:50:20 +0100

    Fetching weather sketch

--- SimpleWeatherApp/AppDelegate.swift
+++ SimpleWeatherApp/AppDelegate.swift
@@ -15,7 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        let conf = Configuration.fromPlist
+        let fetcher = NetworkFetcher(configuration: conf)
+        let locationFetcher = LocationFetcher(fetcher: fetcher)
+        locationFetcher.fetchWeather(forCityID: "4303439") { weather in
+            print("\(String(describing: weather))")
+        }
         return true
     }
 
