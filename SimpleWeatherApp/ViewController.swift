1303b868d29f090c2cc99fb724dc014d5276456b
Justyna Dolińska <justyna@secansoida.com>
2018-01-20 19:41:13 +0100

    Search cities

--- SimpleWeatherApp/ViewController.swift
+++ SimpleWeatherApp/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private var tableView: UITableView!
+
+    private lazy var allCities: [City] = {
+        let cityData = FileManager.default.contents(atPath: Bundle.main.path(forResource: "city.list", ofType: "json")!)!
+        return try! JSONDecoder().decode([City].self, from: cityData)
+    }()
+
+    private var filteredCities: [City] = []
+    private let searchController = UISearchController(searchResultsController: nil)
+    private var lastQuery: String = ""
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search Cities"
+        tableView.tableHeaderView = searchController.searchBar
+        definesPresentationContext = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +39,33 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredCities.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CityCell", for: indexPath)
+        let city = filteredCities[indexPath.row]
+        cell.textLabel?.text = "\(city.name), \(city.country)"
+        return cell
+    }
+}
+
+extension ViewController: UISearchResultsUpdating {
+    // MARK: - UISearchResultsUpdating Delegate
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text, query.count > 2 else {
+            filteredCities = []
+            tableView.reloadData()
+            return
+        }
+        if query.contains(lastQuery) {
+            filteredCities = filteredCities.lazy.filter{ $0.name.contains(query) }
+        } else {
+            filteredCities = allCities.lazy.filter{ $0.name.contains(query) }
+        }
+        tableView.reloadData()
+    }
+}
